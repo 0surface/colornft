e4f473e1a50c72a0bd37248a9d187652117bda2b
SEIFU MEKONNEN TOLOSA <tms.18@yahoo.com>
2021-04-15 12:12:09 +0100

    add minting, tests

--- src/contracts/Color.sol
+++ src/contracts/Color.sol
@@ -3,10 +3,18 @@ pragma solidity 0.5.16;
 import "./ERC721Full.sol";
 
 contract Color is ERC721Full {
-
+    string[] public colors;
+    mapping(string => bool) colorExists;
 
     constructor() ERC721Full("Color","COLOR") public {
+    }
 
+    function mint(string memory _color) public {
+        require(bytes(_color).length == 7);
+        require(!colorExists[_color]);
+        uint _id = colors.push(_color);
+        _mint(msg.sender, _id);
+        colorExists[_color] = true;
     }
 
 }
--- test/color.test.js
+++ test/color.test.js
@@ -1,4 +1,5 @@
 const Color = artifacts.require("Color");
+const truffleAssert = require("truffle-assertions");
 const { assert } = require("chai");
 
 require("chai")
@@ -8,26 +9,59 @@ require("chai")
 contract("Color", (accounts) => {
   let contract;
 
-  before("", async () => {
-    contract = await Color.deployed();
-  });
+  const deployer = accounts[0];
+  const minter = accounts[1];
+
+  const color1 = "#ED097F";
+  const NULL_ADDRESS = "0x0000000000000000000000000000000000000000";
 
   describe("deployment", async () => {
+    before("deploy contract", async () => {
+      contract = await Color.new({ from: deployer });
+    });
+
     it("deploys successfully", async () => {
-      contract = await Color.deployed();
       const _address = contract.address;
       const addressIsValid = _address !== "" && _address !== 0x0 && _address !== null && _address !== undefined;
       assert.isTrue(addressIsValid, "deployed contract address is invald");
     });
 
-    it("has the correct ERC721Metadata name", async () => {
+    it("should have the correct ERC721Metadata name", async () => {
       const name = await contract.name();
       assert.strictEqual(name, "Color");
     });
 
-    it("has the correct ERC721Metadata symbol", async () => {
+    it("should have the correct ERC721Metadata symbol", async () => {
       const symbol = await contract.symbol();
       assert.strictEqual(symbol, "COLOR");
+    });
+  });
+
+  describe("minting", async () => {
+    beforeEach("deploy contract", async () => {
+      contract = await Color.new({ from: deployer });
+    });
+
+    it("should mint a new token", async () => {
+      const txObj = await contract.mint(color1, { from: minter });
+      const totalSupply = await contract.totalSupply();
+      const eventArgs = txObj.logs[0].args;
+
+      assert.strictEqual(Number(totalSupply), 1);
+      assert.strictEqual(eventArgs.tokenId.toNumber(), 1, "incorrect token id");
+      assert.strictEqual(eventArgs.from, NULL_ADDRESS, "incorrect from address");
+      assert.strictEqual(eventArgs.to, minter, "incorrect to address");
+    });
+
+    it("should not mint invalid color", async () => {
+      await truffleAssert.reverts(contract.mint("#E", { from: minter }));
+    });
+
+    it("should not mint existing color", async () => {
+      const txObj = await contract.mint(color1, { from: minter });
+      assert.isDefined(txObj, "transaction not mined");
+
+      await truffleAssert.reverts(contract.mint(color1, { from: minter }));
     });
   });
 });
