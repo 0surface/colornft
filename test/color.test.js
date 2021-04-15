ef644abeb3e236d7507c920b56b1c18cc98bcd1b
SEIFU MEKONNEN TOLOSA <tms.18@yahoo.com>
2021-04-15 11:20:13 +0100

    basic tests

--- test/color.test.js
+++ test/color.test.js
@@ -7,12 +7,27 @@ require("chai")
 
 contract("Color", (accounts) => {
   let contract;
+
+  before("", async () => {
+    contract = await Color.deployed();
+  });
+
   describe("deployment", async () => {
     it("deploys successfully", async () => {
       contract = await Color.deployed();
-      const contractAddress = contract.address;
-      console.log("address", contractAddress);
-      assert.notEqual(contractAddress, "");
+      const _address = contract.address;
+      const addressIsValid = _address !== "" && _address !== 0x0 && _address !== null && _address !== undefined;
+      assert.isTrue(addressIsValid, "deployed contract address is invald");
+    });
+
+    it("has the correct ERC721Metadata name", async () => {
+      const name = await contract.name();
+      assert.strictEqual(name, "Color");
+    });
+
+    it("has the correct ERC721Metadata symbol", async () => {
+      const symbol = await contract.symbol();
+      assert.strictEqual(symbol, "COLOR");
     });
   });
 });
